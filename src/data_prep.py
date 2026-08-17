15e49ebc8ce7fc01dd1cee5ae2d9e3bba0112ab7
javadhossiniytr@gmail.com <javadhossiniytr@gmail.com>
2026-08-17 06:46:48 +0330

    perf: remove, refactor and optimize the code

--- src/data_prep.py
+++ src/data_prep.py
@@ -11,26 +11,21 @@ from pathlib import Path
 # path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_PATH = PROJECT_ROOT / 'DATA' / 'creditcard.csv'
-MODELS_PATH = PROJECT_ROOT / 'models'
-SCALER_PATH = MODELS_PATH / 'scaler.pkl'
 
-MODELS_PATH.mkdir(exist_ok=True)
 
 
 def load_data(file_path=DATA_PATH):
+    
     """
     load dataset
     out -> df
     """
 
     file_path = Path(file_path)
-
     if not file_path.exists():
         raise FileNotFoundError(f'dataset not found {file_path}')
-
     df = pd.read_csv(file_path)
-    print(f'df loaded: {df.shape}\n\n')
-    
+    print(f'\n\ndf loaded...\n df shape: {df.shape}\n')
     return df
 
 
@@ -46,25 +41,18 @@ def explore_data(df):
     class dist;
     check duplicate
     """
-    
+    print(f'=' * 25)
+    print('\nInformation')
     print(f'sampels: {len(df):,}')
-    print(f'features: {len(df.columns)}')
-
-    print('\nmissing values:')
+    print(f'features: {len(df.columns)-1}')
+    print(f'\nmissing values:{df.isnull().sum().sum()}\n')
     print(df.isnull().sum())
-
     print(f'\nDuplicate: {df.duplicated().sum()}')
-
-    print(f"Class dist: {df['Class'].value_counts()}")
-
+    print(f"\nClass dist count: {df['Class'].value_counts()}")
     print(f"\nClass ratio: {df['Class'].value_counts(normalize=True)}")
-
-    print(f'Discribe: {df.describe()}')
-    
-    print(f'info:')
-    
+    print(f'\n\nDiscribe: {df.describe()}')
+    print(f'\ninfo:')
     df.info()
-
     return df
 
 
@@ -80,12 +68,10 @@ def manage_duplicate(df):
     after = len(df)
     removed = before - after
     balance = df['Class'].value_counts(normalize=True)
-
     print(
         f'Befor: {before}, After: {after}, '
         f'Removed: {removed}, \nBalance: {balance}'
     )
-
     return df
 
 
@@ -95,13 +81,10 @@ def prepare_feature_target(df, target_columns):
     out -> separate target|
     gen X, y
     """
-
     X = df.drop(columns=[target_columns])
     y = df[target_columns]
-
     print(f'X.shape:{X.shape}')
     print(f'y.shape:{y.shape}')
-
     return X, y
 
 
@@ -117,49 +100,18 @@ def split_data(X, y, test_size=0.2, random_state=42):
         stratify=y,
         random_state=random_state
     )
-
-    print(f'X-train: {X_train.shape}')
-    print(f'X-test: {X_test.shape}')
-
-    print(f'y train dist{y_train.value_counts(normalize=True)}')
-    print(f'y test dist{y_test.value_counts(normalize=True)}')
+    print('----shape of X, y after split----')
+    print(f"X_train shape: {X_train.shape}")
+    print(f"X_test shape:  {X_test.shape}")
+    print(f"y_train shape: {y_train.shape}")
+    print(f"y_test shape:  {y_test.shape}")
+    print('\n----Distribution--------')
+    print('y train dist',y_train.value_counts(normalize=True))
+    print('y test dist',y_test.value_counts(normalize=True))
 
     return X_train, X_test, y_train, y_test
 
 
-# Scaling with Pipeline
-def scale_features(X_train, X_test):
-
-    pipeline = Pipeline([
-        ('scaler', StandardScaler())
-    ])
-
-    X_train_scaled = pipeline.fit_transform(X_train)
-    X_test_scaled = pipeline.transform(X_test)
-
-    X_train_scaled = pd.DataFrame(
-        X_train_scaled,
-        columns=X_train.columns,
-        index=X_train.index
-    )
-
-    X_test_scaled = pd.DataFrame(
-        X_test_scaled,
-        columns=X_test.columns,
-        index=X_test.index
-    )
-
-    return X_train_scaled, X_test_scaled, pipeline
-
-
-def save_scaler(pipeline, path=SCALER_PATH):
-    """
-    save fitted pipeline
-    """
-
-    joblib.dump(pipeline, path)
-
-    print(f'pipeline saved to: {path}')
 
 
 if __name__ == "__main__":
@@ -173,12 +125,3 @@ if __name__ == "__main__":
     X, y = prepare_feature_target(df, 'Class')
 
     X_train, X_test, y_train, y_test = split_data(X, y)
-
-    X_train_scaled, X_test_scaled, pipeline = scale_features(
-        X_train,
-        X_test
-    )
-
-    save_scaler(pipeline)
-
-    print(f'{100} * 100\nData prep completed.')
