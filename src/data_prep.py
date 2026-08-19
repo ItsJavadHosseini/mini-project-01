50a8b7a655ff7718e6f3278f3aebc310bda799a3
javadhossiniytr@gmail.com <javadhossiniytr@gmail.com>
2026-08-19 13:29:45 +0330

    refactore: improve code structure

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,10 +1,5 @@
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-import joblib
-import os
 from pathlib import Path
 
 
